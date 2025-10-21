693c65f02ff883831357456f9579ad62e16917d4
Neorak <499298+davidweb@users.noreply.github.com>
2025-10-21 23:06:48 +0100

    Delete include directory

--- include/LoRaHandler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-void taskLoRaHandler(void *pvParameters);
-void loraInterrupt();
-
-// Fonctions pour le chiffrement AES
-String encrypt_payload(const String& plaintext);
-String decrypt_payload(const String& b64_ciphertext);
--- include/OledDisplay.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <Arduino.h>
-void taskOledDisplay(void *pvParameters);
